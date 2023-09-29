8111f8668930afea0849001bcc6b132dabe39b04
Mat Moore <MatMoore@users.noreply.github.com>
2023-09-29 09:55:57 +0100

    Make sure deployment is created on the first apply
    This should depend on the method/integration as well as the resource,
    otherwise we get an error saying that no methods are defined on the
    resource.

--- modules/api_endpoints/main.tf
+++ modules/api_endpoints/main.tf
@@ -10,7 +10,9 @@ resource "aws_api_gateway_deployment" "deployment" {
     #       resources will show a difference after the initial implementation.
     #       It will stabilize to only change when resources change afterwards.
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.upload_data
+      aws_api_gateway_resource.upload_data,
+      aws_api_gateway_method.upload_data_get,
+      aws_api_gateway_integration.upload_data_to_lambda
     ]))
   }
 
